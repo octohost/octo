fabc8d1d73a24576f3704d1f6cd8563fb8766875
Darron Froese <darron@froese.org>
2016-01-18 15:41:15 -0700

    Some more testing.

--- test/tests.bash
+++ test/tests.bash
@@ -18,12 +18,57 @@ T_12configget() {
   [[ "$?" -eq "0" ]]
 }
 
-T_14configdel() {
+T_14configdelrm() {
   bin/octo config del -c darron --key="KEY1"
-  bin/octo config del -c darron --key="KEY2"
+  bin/octo config rm -c darron --key="KEY2"
 }
 
 T_16configshow() {
   result="$(bin/octo config show -c darron)"
   [[ "$result" == '/darron/TESTING:"This value is set for the darron container."' ]]
+}
+
+T_18configexport() {
+  result="$(bin/octo config export -c darron)"
+  [[ "$result" == 'octo config set -c="darron" --key="TESTING" --value="This value is set for the darron container."' ]]
+}
+
+T_20containerGetSpace() {
+  result="$(bin/octo config get -c 'there should not')"
+  [[ "$result" == 'A container cannot contain a space.' ]]
+}
+
+T_22getSpace() {
+  result="$(bin/octo config get -c 'darron' --key 'This key should fail.' )"
+  [[ "$result" == 'A key cannot contain a space.' ]]
+}
+
+T_24containerSetSpace() {
+  result="$(bin/octo config set -c 'there should not')"
+  [[ "$result" == 'A container cannot contain a space.' ]]
+}
+
+T_26setSpace() {
+  result="$(bin/octo config set -c 'darron' --key 'This key should fail.' )"
+  [[ "$result" == 'A key cannot contain a space.' ]]
+}
+
+T_28containerDelSpace() {
+  result="$(bin/octo config del -c 'there should not')"
+  [[ "$result" == 'A container cannot contain a space.' ]]
+}
+
+T_30delSpace() {
+  result="$(bin/octo config del -c 'darron' --key 'This key should fail.' )"
+  [[ "$result" == 'A key cannot contain a space.' ]]
+}
+
+T_32containerShow() {
+  result="$(bin/octo config show -c 'there should not')"
+  [[ "$result" == 'A container cannot contain a space.' ]]
+}
+
+T_34containerShow() {
+  result="$(bin/octo config export -c 'there should not')"
+  [[ "$result" == 'A container cannot contain a space.' ]]
 }
